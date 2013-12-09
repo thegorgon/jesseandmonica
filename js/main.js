a50834aa6ae5cd0e09e8cbc303fa319e90c2bdd4
Jesse Reiss <jessereiss@squareup.com>
2013-12-08 22:45:08 -0800

    Fade in

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-$(document).ready(function() {
-  var controller = $.superscrollorama({
-    triggerAtCenter: false,
-    playoutAnimations: true
-  });
-
-  // controller.pin($('#slide-1'), 3000, {
-  //   anim: TweenMax.from($('#title'), 0.5, {css:{opacity:0}})
-  // });
+$(window).load(function() {
+  $('.slide').fadeIn();
 });
