ebda22fd9b0aad85df70ca066bcee9d9285c0f46
Jesse Reiss <jessereiss@squareup.com>
2013-12-08 22:42:18 -0800

    Add collage

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+$(document).ready(function() {
+  var controller = $.superscrollorama({
+    triggerAtCenter: false,
+    playoutAnimations: true
+  });
 
+  // controller.pin($('#slide-1'), 3000, {
+  //   anim: TweenMax.from($('#title'), 0.5, {css:{opacity:0}})
+  // });
+});
