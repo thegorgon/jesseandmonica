9e97014c96a743aa957d80d6dc401b3a2c2ea7f1
Jesse Reiss <jessereiss@squareup.com>
2013-12-27 10:15:07 -0800

    Forget the loader

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 (function() {
-  $('#loader').waitForImages({
-    finished: function() {
-      $('#loader').fadeIn(10);
-    },
-    waitForAll: true
-  });
   var reveal = function() {
-    $('#loader').fadeOut(250);
     $('#container').fadeIn(250);
   };
   $('body').waitForImages({
